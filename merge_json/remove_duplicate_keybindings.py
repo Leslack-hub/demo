bff97214c564369421bb04f226b70a63f7489512
le slack <lishanchuan@nineton.cn>
2025-04-07 15:42:48 +0800

    fix: 增加类型注释

--- merge_json/remove_duplicate_keybindings.py
+++ merge_json/remove_duplicate_keybindings.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
+from typing import List, Dict, Any
 import json
-# import sys
-# import argparse
 from collections import defaultdict
 
 
-def process_keybindings(input_file, output_file):
-    commands = defaultdict(int)
+def process_keybindings(input_file: str, output_file: str) -> None:
+    commands: defaultdict[str, int] = defaultdict(int)
     with open(input_file, 'r') as file:
-        keybindings = json.load(file)  # 返回列表
-    processed = []
+        keybindings: List[Dict[str, Any]] = json.load(file)
+    processed: List[Dict[str, Any]] = []
 
     for item in keybindings:
         if not isinstance(item, dict):
             continue
 
-        command = item.get('command', '')
+        command: str = item.get('command', '')
         if command not in commands:
             processed.append(item)
             commands[command] = len(processed) - 1
         else:
             print(f"重复的command {command}")
-            index = commands.get(command)
+            index: int = commands.get(command)
             processed[index] = item
+
     with open(output_file, 'w') as f:
         json.dump(processed, f, indent=2)
 
